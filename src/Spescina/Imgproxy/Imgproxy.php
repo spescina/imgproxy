6f56824555a651c2064bc782a12258e4d70ebb16
Simone Pescina <s.pescina@gmail.com>
2014-04-11 18:41:19 +0200

    added optional parameters: zoom/crop and quality

--- src/Spescina/Imgproxy/Imgproxy.php
+++ src/Spescina/Imgproxy/Imgproxy.php
@@ -5,10 +5,17 @@ use Illuminate\Support\Facades\URL;
 class Imgproxy {
 
         const PREFIX = 'packages/spescina/imgproxy';
+        
+        const Q = 90;
+        const ZC = 1;
 
-        public function link($path, $width, $height)
+        public function link($path, $width, $height, $quality = null, $zoomCrop = null)
         {
-                $url = array(self::PREFIX, $width, $height, $path);
+                $q = is_null($quality) ? self::Q : $quality;
+                
+                $zc = is_null($zoomCrop) ? self::ZC : $zoomCrop;
+                
+                $url = array(self::PREFIX, $width, $height, $zc, $q, $path);
 
                 return URL::to(implode('/', $url));
         }
--- tests/ImgproxyTest.php
+++ tests/ImgproxyTest.php
@@ -16,14 +16,42 @@ class ImgproxyTest extends PHPUnit_Framework_TestCase {
         {
                 URL::shouldReceive('to')
                         ->once()
-                        ->with("packages/spescina/imgproxy/100/70/image/path/url.jpg")
-                        ->andReturn("http://www.example.com/packages/spescina/imgproxy/100/70/image/path/url.jpg");
+                        ->with("packages/spescina/imgproxy/100/70/1/90/image/path/url.jpg")
+                        ->andReturn("http://www.example.com/packages/spescina/imgproxy/100/70/1/90/image/path/url.jpg");
                 
                 $imgProxy = new Imgproxy;
                 
                 $url = $imgProxy->link("image/path/url.jpg", 100, 70);
                 
-                $this->assertEquals("http://www.example.com/packages/spescina/imgproxy/100/70/image/path/url.jpg", $url);
+                $this->assertEquals("http://www.example.com/packages/spescina/imgproxy/100/70/1/90/image/path/url.jpg", $url);
+        }
+        
+        public function test_image_link_building_with_quality()
+        {
+                URL::shouldReceive('to')
+                        ->once()
+                        ->with("packages/spescina/imgproxy/100/70/1/70/image/path/url.jpg")
+                        ->andReturn("http://www.example.com/packages/spescina/imgproxy/100/70/1/70/image/path/url.jpg");
+                
+                $imgProxy = new Imgproxy;
+                
+                $url = $imgProxy->link("image/path/url.jpg", 100, 70, 70);
+                
+                $this->assertEquals("http://www.example.com/packages/spescina/imgproxy/100/70/1/70/image/path/url.jpg", $url);
+        }
+        
+        public function test_image_link_building_with_quality_and_zoomcrop()
+        {
+                URL::shouldReceive('to')
+                        ->once()
+                        ->with("packages/spescina/imgproxy/100/70/2/70/image/path/url.jpg")
+                        ->andReturn("http://www.example.com/packages/spescina/imgproxy/100/70/2/70/image/path/url.jpg");
+                
+                $imgProxy = new Imgproxy;
+                
+                $url = $imgProxy->link("image/path/url.jpg", 100, 70, 70, 2);
+                
+                $this->assertEquals("http://www.example.com/packages/spescina/imgproxy/100/70/2/70/image/path/url.jpg", $url);
         }
 
 }
